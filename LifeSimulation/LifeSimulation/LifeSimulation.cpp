bfd5e30b247c42b5ce17b8800de49a4986844bb1
Benoit <benoitremy74@gmail.com>
2020-09-10 10:43:07 +0200

    update

--- LifeSimulation/LifeSimulation/LifeSimulation.cpp
+++ LifeSimulation/LifeSimulation/LifeSimulation.cpp
@@ -12,23 +12,29 @@ using namespace std::chrono;
 //int ReserveEau;
 //int foodQte;
 
-int Tiredness = 20;
+int TirednessLevel = 20;
+
+void sleep(Human& human)
+{
+    std::cout << "Enter in sleep state." << std::endl;
+    std::cout << "Human need to sleep." << std::endl;
+    while (human.getTiredness() < TirednessLevel)
+    {
+        sleep_for(2s);
+        human.addTiredness(2);
+        std::cout << "Tiredness: " << human.getTiredness() << std::endl;
+    }
+    std::cout << "Human isn't tired anymore." << std::endl;
+    std::cout << "Moving to an other state." << std::endl;
+}
 
 void humanIsTired(Human& human)
 {
-    if (human.getTiredness() < Tiredness)
+    if (human.getTiredness() < TirednessLevel)
     {
-        std::cout << "Enter in sleep state." << std::endl;
+        std::cout << "Human is tired." << std::endl;
         std::cout << "Tiredness: " << human.getTiredness() << std::endl;
-        std::cout << "Human need to sleep." << std::endl;
-        while (human.getTiredness() < Tiredness)
-        {
-            sleep_for(2s);
-            human.addTiredness(2);
-            std::cout << "Tiredness: " << human.getTiredness() << std::endl;
-        }
-        std::cout << "Human isn't tired anymore." << std::endl;
-        std::cout << "Moving to an other state." << std::endl;
+        sleep(human);
     }
 }
 
