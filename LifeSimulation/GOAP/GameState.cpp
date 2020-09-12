3aa6a42746cb402fe08bb9515e454ea6255db2c6
Benoît <benoitremy74@gmail.com>
2020-09-12 02:38:48 +0200

    update

--- LifeSimulation/GOAP/GameState.cpp
+++ LifeSimulation/GOAP/GameState.cpp
@@ -8,37 +8,37 @@ GameState::GameState() {
 	m_PickAxeAmount = 1;
 }
 
-GameState::GameState(const int SwordAmount, const int MoneyAmount, const int FORGERONAmount, const int IronAmount, const int PickAxeAmount){
+GameState::GameState(const int SwordAmount, const int MoneyAmount, const int FORGERONAmount, const int IronAmount, const int PickAxeAmount) {
 	m_SwordAmount = SwordAmount;
 	m_MoneyAmount = MoneyAmount;
 	m_ForgeronAmount = FORGERONAmount;
 	m_IronAmount = IronAmount;
 	m_PickAxeAmount = PickAxeAmount;
-
 }
 
 void GameState::CreateSword(const int UsedAmount) {
 	m_ForgeronAmount -= UsedAmount;
 	m_IronAmount -= UsedAmount;
 	m_SwordAmount += UsedAmount;
-
 }
+
 void GameState::CreateForgeron(const int UsedAmount) {
 	m_MoneyAmount -= UsedAmount;
 	m_ForgeronAmount += UsedAmount;
-
 }
+
 void GameState::FarmIron(const int UsedAmount) {
 	m_ForgeronAmount -= UsedAmount;
 	m_PickAxeAmount -= UsedAmount;
 	m_IronAmount += UsedAmount;
 }
+
 void GameState::MakePickAxe(const int UsedAmount) {
 	m_MoneyAmount -= UsedAmount;
 	m_IronAmount += UsedAmount;
 }
 
-void GameState::SellSword(const int UsedAmount){
+void GameState::SellSword(const int UsedAmount) {
 	m_SwordAmount -= UsedAmount;
 	m_MoneyAmount += UsedAmount;
 }
